9c60d82fc59bcae956cb24feff801c5c4a0e4faf
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-06-04 08:14:54 -0300

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Igor Florentino on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
